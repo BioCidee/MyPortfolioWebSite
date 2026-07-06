fe1a087e7c33258fa721449ac2788b904c12050e
COURTOIS Rudy <rudy.courtois@hotmail.com>
2026-07-06 12:02:01 +0200

    Nothing

--- js/test.js
+++ js/test.js
@@ -1,14 +1,13 @@
 var buttonSeeGame = document.getElementById("SeeGames");
 
 var buttonPopup = document.getElementById("popup-fermer");
-var buttonProjectMars = document.getElementById("buttonProjectMars");
-var buttonPowerOfD = document.getElementById("buttonPowerOfD");
-var buttonMarsRun = document.getElementById("buttonMarsRun");
+var buttonDiscover = document.querySelectorAll(".buttonDiscover");
 var overlay = document.getElementById("overlay");
 
-function ouvrirPopup(titre, description){
-    document.getElementById("popup-titre").textContent = titre;
-    document.getElementById("popup-description").textContent = description;
+function ouvrirPopup(_titre, _description, _image){
+    document.getElementById("popup-titre").textContent = _titre;
+    document.getElementById("popup-description").textContent = _description;
+    document.getElementById("popup-image").src = _image;
     overlay.style.display = "flex";
 }
 
@@ -17,16 +16,10 @@ buttonSeeGame.addEventListener("click", function() {
 } 
 )
 
-buttonProjectMars.addEventListener("click", function() {
-    ouvrirPopup("Project Mars", "Un jeu de gestion ou du doit développer tes défenses sur Mars, dans un monde en 3d pour te défendre des aliens");
-})
-
-buttonMarsRun.addEventListener("click", function() {
-    ouvrirPopup("Mars Run", "Un infinite runner classque sur Mars ! Disposant de deux type de gameplay ! Un classique et un autre en vue fps !");
-})
-
-buttonPowerOfD.addEventListener("click", function() {
-    ouvrirPopup("Power Of Destruction", "Jeu réaliser pour une game jam ! En seulement 2 jours ! Prenez le contrôle d'un géant et détruiser toute la ville !");
+buttonDiscover.forEach(function(bouton){
+    bouton.addEventListener("click", function(){
+        ouvrirPopup(bouton.dataset.title, bouton.dataset.description, bouton.dataset.image);
+    })
 })
 
 buttonPopup.addEventListener("click", function() {
